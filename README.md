6749156c6bb35a9683c58a82f025abf9e47b3053
Aprilhuu <38861292+Aprilhuu@users.noreply.github.com>
2020-10-01 13:25:28 -0400

    Activity2: Replay and modify example in Chapter 4

--- README.md
+++ README.md
@@ -7,3 +7,24 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky
 <p align="center">
   <img src="./screenshots/Activity1Screenshots.png" />
 </p>
+
+## Activity 2: Replay and modify example in Chapter 4
+Start screen:
+<p align="center">
+  <img src="./screenshots/Activity2Screenshot_1.png" />
+</p>
+
+First name & UofT email:
+<p align="center">
+  <img src="./screenshots/Activity2Screenshot_2.png" />
+</p>
+
+First and last name & First name:
+<p align="center">
+  <img src="./screenshots/Activity2Screenshot_3.png" />
+</p>
+
+First and last name & Non UofT email:
+<p align="center">
+  <img src="./screenshots/Activity2Screenshot_4.png" />
+</p>
--- hello.py
+++ hello.py
@@ -1,15 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'blllllalalallalaeeecagdaf'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email(granular_message=True)])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        old_email = session.get('email')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    email_addr = session.get('email')
+    if email_addr is None:
+        message = None
+    elif email_addr.endswith(".utoronto.ca"):
+        message = "Your UofT email is " + email_addr
+    else:
+        message = "Please use your UofT email."
+    return render_template('index.html', form=form, name=session.get('name'), message=message)
 
 @app.route('/user/<name>')
 def user(name):
